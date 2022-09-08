fe9661696c22119ea5c19ac258e7ab336c1ad49e
David Onuta <ogagaonuta@gmail.com>
2022-09-08 23:53:03 +0000

    C program using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
+/**
+* main - Entry point
+* @void: No argument
+* Return: 0
+*/
+
 int main(void)
 {
 	char x[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(x);
-	return(0);
+	return (0);
 }
