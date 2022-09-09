e21bafd48a05af63fe5d11795dc9f260a4a8ac62
David Onuta <ogagaonuta@gmail.com>
2022-09-09 00:25:37 +0000

    C program using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio>
+
+/**
+* main - Entry point
+* @void: No argument
+* Return: 0
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
