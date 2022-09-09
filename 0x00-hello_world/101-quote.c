d6592249fc70957dd7fa81e44026c8e1e1fdf55d
David Onuta <ogagaonuta@gmail.com>
2022-09-09 04:54:06 +0000

    C program using write

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+* main - Entry point
+* @void: No argument
+*
+* Return: 1
+*/
+
+int main(void)
+{
+	char z[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(STDOUT_FILENO, &z, sizeof(z));
+	return (1);
+}
