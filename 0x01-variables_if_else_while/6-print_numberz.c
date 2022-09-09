b7cee604111e84f686ca65f77a9cfa7bd2483dae
David Onuta <ogagaonuta@gmail.com>
2022-09-09 23:42:41 +0000

    Print numbers 0 to 9 using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* void: No argument
+*
+* Return: 0
+*/
+
+int main(void)
+{
+	int digit = '0';
+
+	while (digit <= '9')
+	{
+		putchar(digit);
+		digit++;
+	}
+
+	putchar('\n');
+	return (0);
+}
