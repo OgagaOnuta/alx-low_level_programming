bb6bd5bc5896250b8472ae5ea92ed43b21d3a9b4
David Onuta <ogagaonuta@gmail.com>
2022-09-10 00:13:46 +0000

    Print Hexadecimal in lowercase

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* void: No argument
+*
+* Return: 0
+*/
+
+int main(void)
+{
+	char hexNum = '0';
+	char hexAlpha = 'a';
+
+	while (hexNum <= '9')
+	{
+		putchar(hexNum);
+		hexNum++;
+	}
+
+	while (hexAlpha <= 'f')
+	{
+		putchar(hexAlpha);
+		hexAlpha++;
+	}
+
+	putchar('\n');
+	return (0);
+}
