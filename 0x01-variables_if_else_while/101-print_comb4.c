83c00d488cdc3a576b2df58395beff0eec954688
David Onuta <ogagaonuta@gmail.com>
2022-09-10 03:36:54 +0000

    Print different combo of three digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,44 +9,38 @@
 
 int main(void)
 {
-	int f1 = '0';
-	int s2 = '1';
-	int t3 = '2';
-	int cntS2 = '0';
-	int cntT3 = '0';
+	int f1 = '0', s2, t3;
+	int F1, S2, T3;
 
-	while (f1 <= '7' && s2 <= '8' && t3 <= '9' && (f1 < s2 && f1 < t3 && s2 < t3))
+	while (f1 <= '7')
 	{
-		cntS2 = s2;
-		while (cntS2 <= '8')
+		s2 = '1';
+		while (s2 <= '8')
 		{
-			cntT3 = t3;
-			while (cntT3 <= '9')
+			t3 = '2';
+			while (t3 <= '9')
 			{
-				putchar(f1);
-				putchar(s2);
-				putchar(t3);
-				if (f1 == '7' && s2 == '8' && t3 == '9')
+				F1 = f1, S2 = s2, T3 = t3;
+				if ((F1 < S2 && S2 < T3) && (F1 != '7'))
 				{
-					putchar('\n');
-					return (0);
-				}
-				else
-				{
+					putchar(F1);
+					putchar(S2);
+					putchar(T3);
 					putchar(',');
 					putchar(' ');
 				}
-				if (f1 == s2 || s2 == t3)
+				if (F1 == '7' && S2 == '8' && T3 == '9')
 				{
-					return (0);
+					putchar(F1);
+					putchar(S2);
+					putchar(T3);
+					putchar('\n');
 				}
-				cntT3++;
+				t3++;
 			}
-			cntS2++;
+			s2++;
 		}
 		f1++;
-		s2++;
-		t3++;
 	}
 	return (0);
 }
