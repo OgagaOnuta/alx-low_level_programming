d232c6aa3da498f60bb6990f0ecb6fa9e74a871e
David Onuta <ogagaonuta@gmail.com>
2022-09-10 00:47:05 +0000

    Print combo of digit and comma using putchar

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* void: No argument
+*
+* Return: 0
+*/
+
+int main(void)
+{
+	int num = '0';
+
+	while (num <= '9')
+	{
+		putchar(num);
+
+		if (num < '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		else
+		{
+			putchar('\n');
+			return (0);
+		}
+
+		num++;
+	}
+
+	return (0);
+}
