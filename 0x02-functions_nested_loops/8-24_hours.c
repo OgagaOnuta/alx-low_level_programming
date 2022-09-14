0f42efd6872f80228bc34d60ce1336334f35874f
David Onuta <ogagaonuta@gmail.com>
2022-09-14 16:16:22 +0000

    Print every minute of the day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,40 +9,27 @@
 
 void jack_bauer(void)
 {
-	int a = 0, a1, a2, a3, a0;
-	int b1, b2, b3, b0;
-	int c2, c3, c0;
-	int d3, d0;
+	int a = 0, a0;
+	int b0;
 
-	while (a <= 2)
+	while (a <= 23)
 	{
-		a1 = a, b1 = 0;
+		a0 = a, b0 = 0;
 
-		while (b1 <= 3)
+		while (b0 <= 59)
 		{
-			a2 = a1, b2 = b1, c2 = 0;
-
-			while (c2 <= 5)
+			a0 = a;
+/*
+			if (a0 <= 9 || b0 <= 9)
 			{
-				a3 = a2, b3 = b2, c3 = c2, d3 = 0;
-
-				while (d3 <= 9)
-				{
-					a0 = a3, b0 = b3, c0 = c3, d0 = d3;
-
-					_putchar(a0 + '0');
-					_putchar(b0 + '0');
-					_putchar(':');
-					_putchar(c0 + '0');
-					_putchar(d0 + '0');
-					_putchar('\n');
-					d3++;
-					continue;
-				}
-				c2++;
-				continue;
+				
 			}
-			b1++;
+*/
+			_putchar(a0 + '0');
+			_putchar(':');
+			_putchar(b0 + '0');
+			_putchar('\n');
+			b0++;
 			continue;
 		}
 		a++;
