405b8e6514f10f72f038ee79988432e6eb71514d
David Onuta <ogagaonuta@gmail.com>
2022-09-14 02:24:38 +0000

    Print lowercase alphabets multiple times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints the alphabets 10 times
+ * @void: No argument
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	char lowAlpha;
+	int i = 0;
+
+	while (i < 10)
+	{
+		lowAlpha = 'a';
+
+		while (lowAlpha <= 'z')
+		{
+			_putchar(lowAlpha);
+			lowAlpha++;
+		}
+	}
+
+	_putchar('\n');
+}
