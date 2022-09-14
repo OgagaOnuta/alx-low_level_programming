1cf27c630597fbb00fa4e23727055a99517449ff
David Onuta <ogagaonuta@gmail.com>
2022-09-14 13:04:18 +0000

    Return absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+
+/**
+ * _abs - Computes the absolute value of an integer
+ * @x: Integer to be tested
+ *
+ * Return: xabs
+ */
+
+int _abs(int x)
+{
+	int xabs = abs(x);
+	return (xabs);
+}
