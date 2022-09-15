d9bae19c0c0e5431adfd2c452c06037da14418b3
David Onuta <ogagaonuta@gmail.com>
2022-09-15 13:08:45 +0000

    Multiply two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+/**
+ * mul - Multiplies two integers
+ * @a, @b: Arguments to be multiplied
+ *
+ * Return: result of multiplication
+ */
+
+int mul(int a, int b)
+{
+	int resMul;
+
+	resMul = a * b;
+	return (resMul);
+}
