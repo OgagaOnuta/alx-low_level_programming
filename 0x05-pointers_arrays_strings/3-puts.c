d855d2962f47d73959ededc7aacb0c6d7b0d75d4
David Onuta <ogagaonuta@gmail.com>
2022-09-20 03:35:32 +0000

    Prints a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - Prints a string, followed by a new line, to stdout
+ * @str: String to print
+ *
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+}
