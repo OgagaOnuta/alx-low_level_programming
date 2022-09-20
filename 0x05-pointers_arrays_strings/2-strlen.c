27380d167135203ba27a6c2b993cede217696579
David Onuta <ogagaonuta@gmail.com>
2022-09-20 03:34:29 +0000

    Returns the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strlen - Calculates the length of a string
+ * @s: String
+ *
+ * Return: Length of string
+ */
+
+int _strlen(char *s)
+{
+	int xLength = strlen(s);
+
+	return (xLength);
+}
