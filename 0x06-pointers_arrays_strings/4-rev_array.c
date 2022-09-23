7470157a4f5f680fb2a6b4d0723f7e0a80235120
David Onuta <ogagaonuta@gmail.com>
2022-09-22 18:46:33 -0700

    Reverse the content of an array of integers

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,38 @@
+/**
+ * swap_int - Swaps the values of two integers
+ * @a: Argument to be swapped
+ * @b: Argument to be swapped
+ *
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c;
+	int d;
+
+	c = *a;
+	d = *b;
+	*a = d;
+	*b = c;
+}
+
+/**
+ * reverse_array - Reverses the content of an array of integers
+ * @a: Array
+ * @n: Number of elements in the array
+ *
+ * Return: void
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i < (n / 2))
+	{
+		int *ap = &a[i], *bp = &a[n - i - 1];
+		swap_int(ap, bp);
+		i++;
+	}
+}
