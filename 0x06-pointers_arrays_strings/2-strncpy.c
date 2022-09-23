30ffbe6c04f93dbe3cd3e2c6356bbccc0b264588
David Onuta <ogagaonuta@gmail.com>
2022-09-22 17:52:17 -0700

    Copy a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,15 @@
+#include <string.h>
+
+/**
+ * _strncpy - Copies a string
+ * @dest: String to be copied to
+ * @src: String to be copied
+ * @n: Not more than n bytes shall be copied
+ *
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	dest = strncpy(dest, src, n);
+	return (dest);
+}
