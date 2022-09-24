27839ae984ebe0fae654ba44533c9c11357e328a
David Onuta <ogagaonuta@gmail.com>
2022-09-24 19:12:47 +0000

    Capitalize all words of a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+/**
+ * cap_string - Capitalizes all words of a string
+ * @s: String to be capitalized
+ *
+ * Return: Capitalized string
+ */
+
+char *cap_string(char *s)
+{
+	int i = 0, j;
+	char separator[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+
+	if (s[0] >= 'a' && s[0] <= 'z')
+	{
+		s[0] -= 32;
+	}
+
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (j < 13)
+		{
+			if (s[i] == separator[j])
+			{
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				{
+					s[i + 1] -= 32;
+				}
+			}
+			j++;
+		}
+		i++;
+	}
+	return (s);
+}
