7d2c253a2889c4ae35a1e846f9ae4766306e682f
David Onuta <ogagaonuta@gmail.com>
2022-09-30 17:42:34 +0000

    Add positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Adds positve numbers.
+ * @argc: Argument count
+ * @argv: Argument vector
+ *
+ * Return: 0 if success, 1 if fail.
+ */
+
+int main(int argc, char **argv)
+{
+	int i, add;
+
+	if (argc == 1)
+	{
+		add = 0;
+	}
+	else if(argc > 1)
+	{
+		i = 1, add = 0;
+		while (i < argc)
+		{
+			if (atoi(argv[i]) == 0)
+			{
+				printf("Error\n");
+				return (1);
+			}
+			else
+			{
+				add += atoi(argv[i]);
+				i++;
+			}
+		}
+	}
+
+	printf("%d\n", add);
+	return (0);
+}
