4b8d70b6be755348151170cb984e2a19ac79ed47
David Onuta <ogagaonuta@gmail.com>
2022-10-04 12:23:33 +0000

    Copy string to new memory space

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup - Copy string to new memory space
+ * @str: String to be copied
+ *
+ * Return: Pointer to string if success, NULL if fail.
+ */
+
+char *_strdup(char *str)
+{
+	char *copyString = malloc(sizeof(char) * strlen(str));
+	long unsigned int i = 0;
+
+	if (copyString == NULL || str == NULL)
+	{
+		return (NULL);
+	}
+
+	while (i < strlen(str))
+	{
+		copyString[i] = str[i];
+		i++;
+	}
+	return (copyString);
+}
