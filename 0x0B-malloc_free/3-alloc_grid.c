9669914060c37720633c1bed9c433df06bc899ff
David Onuta <ogagaonuta@gmail.com>
2022-10-07 04:00:03 +0000

    Create print function

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -28,7 +28,7 @@ int **alloc_grid(int width, int height)
 	while (i < height)
 	{
 		j = 0;
-		twoDimArray[i] = malloc(sizeof(int *) * width);
+		twoDimArray[i] = malloc(sizeof(int) * width);
 		
 		if (twoDimArray[i] == NULL)
 		{
