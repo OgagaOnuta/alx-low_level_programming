6271a6633404e493f747daf58eb851b12020b0f3
David Onuta <ogagaonuta@gmail.com>
2022-10-08 06:59:35 +0000

    Concatenate all arguments of a program separated by a new line

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -22,7 +22,7 @@ char *argstostr(int ac, char **av)
 		i++;
 	}
 
-	conString = malloc(sizeof(char *) * (sizeof(av) * len));
+	conString = malloc(sizeof(char *) * len);
 	if (conString == NULL || ac == 0 || av == NULL)
 	{
 		return (NULL);
