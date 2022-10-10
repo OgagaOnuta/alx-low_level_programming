1bf72bd1216af224d98febeea22408283d5812f6
David Onuta <ogagaonuta@gmail.com>
2022-10-10 23:47:10 +0000

    Initialize a variable of structure

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "dog.h"
 
 /**
@@ -12,7 +13,11 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	struct dog mastiff;
+
+	mastiff.name = name;
+	mastiff.age = age;
+	mastiff.owner = owner;
+
+	*d = mastiff;
 }
