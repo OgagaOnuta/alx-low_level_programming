fa73e510ea1df07b3f675c1562b9896dec61b436
David Onuta <ogagaonuta@gmail.com>
2022-10-12 21:15:01 +0000

    Execute function on each element of array

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,9 +13,12 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i = 0;
 
-	while (i < size)
+	if (action && array)
 	{
-		action(array[i]);
-		i++;
+		while (i < size)
+		{
+			action(array[i]);
+			i++;
+		}
 	}
 }
