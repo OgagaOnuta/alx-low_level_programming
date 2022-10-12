c4b10c7a74a1fb66f42193ef2632a306201e6891
David Onuta <ogagaonuta@gmail.com>
2022-10-12 21:14:07 +0000

    Print a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+/**
+ * print_name - Prints a name
+ * @name: Name to be printed
+ * @f: Function pointer
+ *
+ * Return: void
+ */
+
+void print_name(char *name, void(*f)(char *))
+{
+	f(name);
+}
