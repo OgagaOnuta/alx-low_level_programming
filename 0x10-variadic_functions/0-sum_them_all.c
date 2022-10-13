41f77482f479ae37eb63ef2912d877dbcb665b53
David Onuta <ogagaonuta@gmail.com>
2022-10-13 21:02:12 +0000

    Sum all parameters

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,21 +9,21 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list firstArg;
+	va_list argPtr;
 	unsigned int i = 0;
 	int sum = 0;
 
 	if (n == 0)
 		return (0);
 
-	va_start(firstArg, n);
+	va_start(argPtr, n);
 
 	while (i < n)
 	{
-		sum += va_arg(firstArg, int);
+		sum += va_arg(argPtr, int);
 		i++;
 	}
-	va_end(firstArg);
+	va_end(argPtr);
 
 	return (sum);
 }
