72bceb03cd0cf899a7fcdf9d3f3fdbd4c81f5612
David Onuta <ogagaonuta@gmail.com>
2022-10-13 21:04:10 +0000

    Add header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,5 @@
-#ifndef __VARIADIC_FUNCTIONS_H__
-#define __VARIADIC_FUNCTIONS_H__
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
