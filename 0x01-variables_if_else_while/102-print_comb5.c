663e78e6e11d0b3823f59b8119cb4afd097cb560
David Onuta <ogagaonuta@gmail.com>
2022-10-21 16:26:59 +0000

    Print different combo of four digits

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,4 +9,42 @@
 
 int main(void)
 {
+	int f1, s2, t3, f4;
+
+	f1 = '0';
+	while (f1 <= '9')
+	{
+		s2 = '0';
+		while (s2 <= '8')
+		{
+			t3 = '0';
+			while (t3 <= '9')
+			{
+				f4 = '1';
+				while (f4 <= '9')
+				{
+					if (f4 > s2)
+					{
+					putchar(f1);
+					putchar(s2);
+					putchar(' ');
+					putchar(t3);
+					putchar(f4);
+					if (f1 == '9' && s2 == '8' && t3 == '9')
+						putchar('\n');
+					else
+					{
+						putchar(',');
+						putchar(' ');
+					}
+					}
+					f4++;
+				}
+				t3++;
+			}
+			s2++;
+		}
+		f1++;
+	}
+	return (0);
 }
