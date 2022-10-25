048ac5bb0fabc2e32802fa9a2355cc9c1b1d7b62
David Onuta <ogagaonuta@gmail.com>
2022-10-25 14:06:22 +0000

    Insert a new node at a given position in a linked list

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -31,7 +31,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		}
 		new->n = n;
 
-		/* Locate the node before the given index */		
+		/* Locate the node before the given index */
 		current = *head;
 		x = 0;
 		while (x < (idx - 1))
