43ef01cc9c9e6acd608cebca1699be6d0711c419
David Onuta <ogagaonuta@gmail.com>
2022-10-25 16:36:04 +0000

    Delete node at index of a linked list

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -4,7 +4,7 @@
 size_t lenlist(listint_t *h);
 
 /**
- * delete_nodeint_at_index - Deletes the node at index of a listint_t linked list
+ * delete_nodeint_at_index - Deletes node at index of a listint_t linked list
  * @head: Pointer to listint_t pointer
  * @index: Index of the list where node should be deleted
  *
@@ -13,28 +13,33 @@ size_t lenlist(listint_t *h);
 
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *deleteNode;
-	unsigned int len;
+	listint_t *deleteNode, *tmp;
+	unsigned int x, len;
 
 	len = lenlist(*head);
-	if (len >= index)
+	if (*head == NULL || len < index)
 	{
-		unsigned int x;
+		return (-1);
+	}
 
+	deleteNode = *head; /* Store head node somewhere temporarily */
+	x = 0;
+	if (index == 0) /* If index is at 0, then it's the head node */
+	{
+		*head = (*head)->next; /* Move head node to the next node */
+		free(deleteNode); /* Free head node */
+	}
+	else
+	{
 		/* Locate node before index */
-		deleteNode = *head;
-		x = 0;
-		while (x < index)
+		while (x < (index - 1))
 		{
 			deleteNode = deleteNode->next;
 			x++;
 		}
-		deleteNode->next = deleteNode->next->next; /* Point located node to next two nodes */
-		/* free(deleteNode); */
-	}
-	else
-	{
-		return (-1);
+		tmp = deleteNode->next; /* Store located node somewhere temporarily */
+		deleteNode->next = deleteNode->next->next; /* Point node to next two nodes */
+		free(tmp); /* Free temporary node */
 	}
 
 	return (1);
