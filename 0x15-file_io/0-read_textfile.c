bfb46835921db5fe6f16e08fd6df9e2f2fd73e02
David Onuta <ogagaonuta@gmail.com>
2022-10-31 22:02:28 +0000

    Read a text file and print it to the POSIX stdout

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,47 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * read_textfile - Reads a text file and prints it to the POSIX stdout
+ * @filename: Name of the file
+ * @letters: Number of letters it should read and print
+ *
+ * Return: Actual number of letters it could read and print on success,
+ * 0 on failure
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fdesc;
+	ssize_t numLettR, numLettW;
+	char *buffer;
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
+	if (filename == NULL)
+		return (0);
+
+	fdesc = open(filename, O_RDONLY);
+	if (fdesc == -1)
+		return (0);
+
+	numLettR = read(fdesc, buffer, letters);
+	if (numLettR == -1)
+		return (0);
+
+	close(fdesc);
+
+	numLettW = write(STDOUT_FILENO, buffer, letters);
+	write(STDOUT_FILENO, buffer, letters);
+	if (numLettW == -1)
+		return (0);
+
+	free(buffer);
+	return (numLettR);
+}
