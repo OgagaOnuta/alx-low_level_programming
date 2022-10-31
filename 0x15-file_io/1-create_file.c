ae32e8a2e50e6a63532a451dc2473d9021e1ea32
David Onuta <ogagaonuta@gmail.com>
2022-10-31 23:07:03 +0000

    Create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -28,9 +28,12 @@ int create_file(const char *filename, char *text_content)
 	if (fdesc == -1)
 		return (-1);
 
-	numByte = write(fdesc, text_content, len);
-	if (numByte == -1)
-		return (-1);
+	if (text_content)
+	{
+		numByte = write(fdesc, text_content, len);
+		if (numByte == -1)
+			return (-1);
+	}
 
 	close(fdesc);
 	return (1);
