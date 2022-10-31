64de1c55222148775575cfe2a0e38786cec9a570
David Onuta <ogagaonuta@gmail.com>
2022-10-31 23:43:18 +0000

    Append text at the end of a file

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,66 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include <unistd.h>
+
+int _strlen(char *s);
+
+/**
+ * append_text_to_file - Appends text at the end of a file
+ * @filename: Name of the file
+ * @text_content: NULL terminated string to add at the end of the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fdesc, len;
+	ssize_t numByte;
+
+	len = _strlen(text_content);
+
+	if (filename == NULL)
+		return (-1);
+
+	fdesc = open(filename, O_WRONLY | O_APPEND);
+	if (fdesc == -1)
+		return (-1);
+
+	if (text_content == NULL)
+	{
+		text_content = "";
+		if (filename)
+			return (1);
+		else
+			return (-1);
+	}
+
+	numByte = write(fdesc, text_content, len);
+	if (numByte == -1)
+		return (-1);
+
+	close(fdesc);
+	return (1);
+}
+
+/**
+ * _strlen - Calculates the length of a string
+ * @s: String
+ *
+ * Return: Length of string
+ */
+
+int _strlen(char *s)
+{
+	int xLength;
+
+	xLength = 0;
+	while (s[xLength] != '\0')
+	{
+		xLength++;
+	}
+
+	return (xLength);
+}
