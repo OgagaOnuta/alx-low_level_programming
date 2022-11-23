a230897def1b5394f30088b1d2ed25995318c134
David Onuta <ogagaonuta@gmail.com>
2022-11-23 00:15:34 +0000

    Print an integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,36 +9,16 @@
 
 void print_number(int n)
 {
-	int w, x, y, z;
-
 	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
 	}
 
-	w = n / 1000;
-	x = (n / 100) % 10;
-	y = (n / 10) % 10;
-	z = n % 10;
-	if (n < 10)
-		_putchar(n + '0');
-	else if (n > 10 && n < 100)
+	if (n / 10)
 	{
-		_putchar(y + '0');
-		_putchar(z + '0');
+		print_number(n / 10);
 	}
-	else if (n > 100 && n < 1000)
-	{
-		_putchar(x + '0');
-		_putchar(y + '0');
-		_putchar(z + '0');
-	}
-	else if (n > 1000 && n < 10000)
-	{
-		_putchar(w + '0');
-		_putchar(x + '0');
-		_putchar(y + '0');
-		_putchar(z + '0');
-	}
+
+	_putchar((n % 10) + '0');
 }
