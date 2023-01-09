17b465283ccdeb70bcc8c606c7493b7f21196e4e
David Onuta <ogagaonuta@gmail.com>
2023-01-09 00:11:17 +0000

    Create a hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,20 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Creates a hash table
+ * @size: Size of the array
+ *
+ * Return: Pointer to newly created hash table on SUCCESS
+ * NULL on FAILURE
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table;
+
+	table = malloc(sizeof(hash_table_t) * size);
+	if (table == NULL)
+		return (NULL);
+
+	return (table);
+}
