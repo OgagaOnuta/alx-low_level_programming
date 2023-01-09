611a9494afc80a226fd03a1b8c03b5e633dbdf50
David Onuta <ogagaonuta@gmail.com>
2023-01-09 00:11:17 +0000

    Create a hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,13 +11,23 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
+	unsigned long int index;
 
 	table = malloc(sizeof(hash_table_t *) * size);
 	if (table == NULL)
 		return (NULL);
 
 	table->size = size;
-	table->array = NULL;
+	table->array = calloc(size, sizeof(hash_node_t *));
+	if (table->array == NULL)
+		return (NULL);
+
+	index = 0;
+	while (index < size)
+	{
+		table->array[index] = NULL;
+		index++;
+	}
 
 	return (table);
 }
